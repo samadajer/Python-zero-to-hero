0fa485701dbd5fbb5b7c047e203059ef4fced840
samadajer <93056225+samadajer@users.noreply.github.com>
2021-11-05 10:26:09 +0100

    Add files via upload

---
+++ Python class Task 1.py
@@ -0,0 +1,23 @@
+print("hello", "how are you")
+x = ("apple", "banana", "cherry")
+print(x)
+name = ("Abdulsamad")
+siblings = (3)
+print(name)
+print(siblings)
+
+x = ("Rice and beans") #favorite food
+print(x)
+print(type(x))
+y = (10) #numbers of letters in name
+print(y)
+print(type(y))
+x = (25/19) #age divided by 19
+print(x)
+print(type(x))
+y = 2 < 9
+print(y)
+print(type(y))
+x = 14 > 100
+print(x)
+print(type(x))
---
+++ python class task 1 cont.py
@@ -0,0 +1,43 @@
+#This is a single line comment
+print("Hello, World!")
+print("Hello, World!") #This is a comment after a code
+#print("Hello, World!") We can comment out a code
+print("Cheers, Mate!")
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+"""
+This is a multiline comment
+written within triple quotes
+in more than just one line
+"""
+print("Hello, World!")
+#Arithmetic operators task
+num1 = 12
+num2 = 10
+add_two_numbers = 12 + 10
+print(add_two_numbers)
+#play around task
+x = 50.125 * 2 #multiplication
+print(x)
+y = 200.5/2 #division
+print(y)
+x = 4.645**3 #exponentiation
+print(x)
+y = 50.125 + 50.125 #addition
+print(y)
+x = 200.5 - 100.25 #substraction
+print(x)
+#What is the type of the result of the expression
+x = 3 + 1.5 + 4
+print(x)
+print(type(x))
+#What would you use to find a number’s square root, as well as its square?
+#exponent code
+x = 12**2
+print(x)
+
+
+
+
