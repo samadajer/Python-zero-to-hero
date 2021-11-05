eb7b9f29e672ed1668c06e702897d3dffefcd860
Akeem Lagundoye <38335545+Aifedayo@users.noreply.github.com>
2021-11-05 15:50:49 +0100

    Update full name.py

--- full name.py
+++ full name.py
@@ -1,4 +1,8 @@
+"""CODE LGTM """
+
 firstname = (input("what is your first name : "))
 lastname = (input("what is your last name : "))
 fullname = (firstname + lastname)
 print(fullname)
+
+#Always add context to your print function
